6dccbc8f33752d88a5b14346136215afcf0a87c1
Robert K. Bell <robert.k.bell@gmail.com>
2022-03-01 11:20:56 +1100

    keep the other hdd mounted

--- configuration.nix
+++ configuration.nix
@@ -119,6 +119,24 @@
     in [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
   };
 
+  fileSystems."/mnt/smiticia" = {
+    device = "//192.168.1.98/smiticia";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts =
+        "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      /* ./smb-secrets should look like:
+         ```
+         username=rkb
+         domain=workgroup
+         password=YOURPASSWORDHERE
+         ```
+      */
+    in [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+  };
+
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
