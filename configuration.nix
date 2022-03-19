2144c3e99a3bb5772b3f8dd5f3b031cabbfe86c7
Robert K. Bell <robert.k.bell@gmail.com>
2022-03-20 09:55:58 +1100

    style: fix indent with nixfmt

--- configuration.nix
+++ configuration.nix
@@ -258,12 +258,12 @@
   programs.bash.interactiveShellInit = ''
     source ${pkgs.autojump}/share/autojump/autojump.bash
     j() {
-    if [[ "$#" -ne 0 ]]; then
-        cd $(autojump $@)
-        return
-    fi
-    cd "$(autojump -s | sort -k1gr | awk '$1 ~ /[0-9]:/ && $2 ~ /^\// { for (i=2; i<=NF; i++) { print $(i) } }' |  fzf --height 40% --reverse --inline-info)" 
-}
+        if [[ "$#" -ne 0 ]]; then
+            cd $(autojump $@)
+            return
+        fi
+        cd "$(autojump -s | sort -k1gr | awk '$1 ~ /[0-9]:/ && $2 ~ /^\// { for (i=2; i<=NF; i++) { print $(i) } }' |  fzf --height 40% --reverse --inline-info)"
+    }
   '';
 
   # this might prove useful to debug nix package builds?
