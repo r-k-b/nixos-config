840e4c2187ccd4a720c1023fa7ab04eb95ea8ad7
Robert K. Bell <robert.k.bell@gmail.com>
2023-02-27 10:23:41 +1100

    make the mounted content easier to manage

--- configuration.nix
+++ configuration.nix
@@ -210,7 +210,8 @@
     options = let
       # this line prevents hanging on network split
       automount_opts =
-        "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"
+        + ",uid=rkb,gid=users";
 
       /* ./smb-secrets should look like:
          ```
