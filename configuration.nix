e9f406b42346855ef3f56856210ec6048bfb7294
Robert K. Bell <robert.k.bell@gmail.com>
2023-11-14 09:17:55 +1100

    try to be a little less janky, perf-wise

--- configuration.nix
+++ configuration.nix
@@ -56,6 +56,8 @@
     '';
   };
 
+  powerManagement.cpuFreqGovernor = "performance";
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
